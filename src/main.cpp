6887559269abacf9fb184c66bbf8f657318b8c65
Samuel Brown <samuelbrown3199@gmail.com>
2025-08-18 23:29:12 +0100

    Added required external libraries and linked them
    Also added a temporary SDL window that can be closed. Placeholder!

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
+#include <SDL3/SDL.h>
+
 int main()
 {
 	std::cout << "Hello, World!" << std::endl;
+
+	SDL_Window* window = SDL_CreateWindow("Hello World", 800, 600, SDL_WINDOW_VULKAN);
+	if (!window) {
+		std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	bool run = true;
+	SDL_Event event;
+	while (run)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_EVENT_QUIT)
+			{
+				run = false;
+				break;
+			}
+		}
+	}
+
 	return 0;
 }
