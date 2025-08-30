b5c48ed37eedc5603dbca6a3fd5edde3928f82cc
Samuel Brown <samuelbrown3199@gmail.com>
2025-08-30 19:59:32 +0100

    Add performance visualization to PerformanceStatsUI
    Enhanced the PerformanceStatsUI class by introducing two new member variables: maxFrameTimes, frameTimes, and fpsTimes for tracking frame time and FPS data. Updated the DoInterface method to manage these vectors and prevent overflow. Added ImPlot visualizations for frame times and FPS in the ImGui window, complete with axis limits and labels.

--- src/Interface/PerformanceStatsUI.hpp
+++ src/Interface/PerformanceStatsUI.hpp
@@ -8,6 +8,10 @@ class PerformanceStatsUI : public ToolUI
 
 	PerformanceStats* performanceStats;
 
+	const int maxFrameTimes = 1000;
+	std::vector<float> frameTimes;
+	std::vector<float> fpsTimes;
+
 public:
 
 	void SetPerformanceStats(PerformanceStats* stats)
@@ -17,11 +21,41 @@ public:
 
 	void DoInterface() override
 	{
-		ImGui::Begin("Performance Stats", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+		frameTimes.push_back(performanceStats->GetPerformanceMeasurement("Frame")->GetPerformanceMeasurementInMilliseconds());
+		if (frameTimes.size() > maxFrameTimes)
+			frameTimes.erase(frameTimes.begin());
+
+		fpsTimes.push_back(performanceStats->GetFPS());
+		if(fpsTimes.size() > maxFrameTimes)
+			fpsTimes.erase(fpsTimes.begin());
+
+		ImGui::Begin("Performance Stats", nullptr, ImGuiWindowFlags_None);
 
 		ImGui::Text("Frame Time: %.2f ms", performanceStats->GetPerformanceMeasurement("Frame")->GetPerformanceMeasurementInMilliseconds());
 		ImGui::Text("FPS: %.1f", performanceStats->GetFPS());
 		ImGui::Text("AVG FPS: %.1f", performanceStats->GetAvgFPS());
+
+		if (ImPlot::BeginPlot("Frame Times", ImVec2(-1, 150)))
+		{
+			ImPlot::SetupAxes("Frame", "Time (ms)");
+			ImPlot::SetupAxisLimits(ImAxis_X1, 0, maxFrameTimes, ImGuiCond_Always);
+			ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 50, ImGuiCond_Always);
+
+			// frameTimes is a std::vector<float> containing your frame times
+			ImPlot::PlotLine("Frame Time", frameTimes.data(), frameTimes.size());
+
+			ImPlot::EndPlot();
+		}
+
+		if(ImPlot::BeginPlot("FPS", ImVec2(-1, 150)))
+		{
+			ImPlot::SetupAxes("Frame", "FPS");
+			ImPlot::SetupAxisLimits(ImAxis_X1, 0, maxFrameTimes, ImGuiCond_Always);
+			ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 200, ImGuiCond_Always);
+			// frameTimes is a std::vector<float> containing your frame times
+			ImPlot::PlotLine("FPS", fpsTimes.data(), fpsTimes.size());
+			ImPlot::EndPlot();
+		}
 
 		ImGui::End();
 	}
